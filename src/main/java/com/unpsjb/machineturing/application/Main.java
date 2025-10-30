62308050b025e2a7d47dff4d8190944da8918bef
BrandonAmarillo <brandonxdamarillo@gmail.com>
2025-10-30 16:30:42 -0300

    Estructura de paquetes y maven como herramienta de gestión

---
+++ src/main/java/com/unpsjb/machineturing/application/Main.java
@@ -0,0 +1,8 @@
+package com.unpsjb.machineturing.application;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Machine Turing Application!");
+    }
+}
+
